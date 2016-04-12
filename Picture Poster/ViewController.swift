c084bda7cbc4d891c6e2da65a4ca2a902df8b3e0
Kyle Lee <kiloloco@me.com>
2016-04-12 13:22:29 -0700

    Initial Commit

---
+++ Picture Poster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picture Poster
+//
+//  Created by Kyle Lee on 4/12/16.
+//  Copyright © 2016 Kilo Loco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
